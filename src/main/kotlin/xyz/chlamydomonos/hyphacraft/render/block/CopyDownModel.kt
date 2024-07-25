79a16cfce9ba8d45fa2110ec0054f8a88d988590
Chlamydomonos <2457242458@qq.com>
2024-07-25 22:30:27 +0800

    temp

---
+++ src/main/kotlin/xyz/chlamydomonos/hyphacraft/render/block/CopyDownModel.kt
@@ -0,0 +1,6 @@
+package xyz.chlamydomonos.hyphacraft.render.block
+
+import net.minecraft.client.resources.model.BakedModel
+
+class CopyDownModel(private val defaultModel: BakedModel) : BakedModel by defaultModel {
+}
