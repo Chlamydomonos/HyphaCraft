143b7bdfd0803c040381d74847feffc258bc1e46
Chlamydomonos <2457242458@qq.com>
2024-08-16 12:56:25 +0800

    fix some blocks

--- src/main/kotlin/xyz/chlamydomonos/hyphacraft/blocks/RottenGooBlock.kt
+++ src/main/kotlin/xyz/chlamydomonos/hyphacraft/blocks/RottenGooBlock.kt
@@ -7,13 +7,10 @@ import net.minecraft.world.effect.MobEffects
 import net.minecraft.world.entity.Entity
 import net.minecraft.world.entity.LivingEntity
 import net.minecraft.world.level.BlockGetter
-import net.minecraft.world.level.Explosion
 import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.LiquidBlock
 import net.minecraft.world.level.block.state.BlockState
-import xyz.chlamydomonos.hyphacraft.loaders.BlockLoader
-import xyz.chlamydomonos.hyphacraft.loaders.DamageTypeLoader
 import xyz.chlamydomonos.hyphacraft.loaders.FluidLoader
 
 class RottenGooBlock : LiquidBlock(
@@ -34,16 +31,4 @@ class RottenGooBlock : LiquidBlock(
     override fun getFlammability(state: BlockState, level: BlockGetter, pos: BlockPos, direction: Direction) = 5
 
     override fun getFireSpreadSpeed(state: BlockState, level: BlockGetter, pos: BlockPos, direction: Direction) = 5
-
-    override fun onBlockExploded(state: BlockState, level: Level, pos: BlockPos, explosion: Explosion) {
-        if (level.isClientSide) {
-            return super.onBlockExploded(state, level, pos, explosion)
-        }
-        if (DamageTypeLoader.HYPHA_EXPLOSION(level).key?.let { explosion.damageSource.`is`(it) } == true) {
-            level.setBlock(pos, BlockLoader.HARDENED_FUNGUS_SHELL.block.defaultBlockState(), 3)
-            return
-        }
-
-        super.onBlockExploded(state, level, pos, explosion)
-    }
 }
